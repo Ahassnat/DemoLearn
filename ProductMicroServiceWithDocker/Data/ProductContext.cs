f92369fd0b44a2bc6a4bb628d66b17a17ec53634
Ahassnat <alhassnatabdallah@gmail.com>
2021-10-02 21:50:47 +0300

    Intilize the project file

---
+++ ProductMicroServiceWithDocker/Data/ProductContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductMicroServiceWithDocker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductMicroServiceWithDocker.Data
+{
+    public class ProductDataContext:DbContext
+    {
+        public ProductDataContext(DbContextOptions<ProductDataContext> options) : base(options)
+        {
+        }
+       
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "Electronics",
+                    Description = "Electronic Items",
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Clothes",
+                    Description = "Dresses",
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Grocery",
+                    Description = "Grocery Items",
+                }
+            );
+        }
+
+    }
+}
+
---
+++ ProductMicroServiceWithDocker/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductMicroServiceWithDocker.Models
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
